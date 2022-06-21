5c997a86b47afeba2c742647954aa433b3b4f5a1
Shiven655 <kshiven655@gmail.com>
2022-06-21 21:37:32 +0530

    block-BNaadt

---
+++ block-BNaadt/login-app/models/user.js
@@ -0,0 +1,35 @@
+let mongoose = require('mongoose');
+let bcrypt = require('bcrypt');
+
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, minlength: 4, required: true },
+    age: Number,
+    phone: Number,
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', function (next) {
+  if (this.password && this.isModified('password')) {
+    bcrypt.hash(this.password, 10, (err, hashedValue) => {
+      if (err) {
+        return next(err);
+      }
+      this.password = hashedValue;
+      return next();
+    });
+  } else {
+    next();
+  }
+});
+userSchema.methods.verifyPassword = function (password, cb) {
+  bcrypt.compare(password, this.password, (err, result) => {
+    return cb(err, result);
+  });
+};
+module.exports = mongoose.model('User', userSchema);
